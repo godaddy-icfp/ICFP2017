a08bd3cc09fc637d5922e7ff08be06fee7c2b2f5
John Kercheval <jkercheval@godaddy.com>
2017-08-04 16:35:49 -0700

    Minor update to main to allow stdin to be specified as a file parameter

--- src/main/java/com/godaddy/icfp2017/Application.java
+++ src/main/java/com/godaddy/icfp2017/Application.java
@@ -4,8 +4,14 @@ import com.godaddy.icfp2017.services.GameDriver;
 import com.godaddy.icfp2017.services.GameLogic;
 import com.godaddy.icfp2017.services.GameLogicImpl;
 
+import java.io.FileInputStream;
+
 public class Application {
   public static void main(String[] args) throws Exception {
+
+    if(args.length > 0) {
+      System.setIn(new FileInputStream(args[0]));
+    }
 
     GameLogic gameLogic = new GameLogicImpl();
 
