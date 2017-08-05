93a55f503d985a375bbffdb33a6cc9a4e6ec00b4
tim <teemobean@gmail.com>
2017-08-04 22:06:48 -0700

    Add algorithm weights to rivers.

--- src/main/java/com/godaddy/icfp2017/models/River.java
+++ src/main/java/com/godaddy/icfp2017/models/River.java
@@ -2,12 +2,17 @@ package com.godaddy.icfp2017.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.HashMap;
+
 public class River {
   @JsonProperty("source")
   private int source;
 
   @JsonProperty("target")
   private int target;
+
+  @JsonProperty("algorithmWeights")
+  private HashMap<String, Double> algorithmWeights;
 
   public int getSource() {
     return source;
@@ -24,4 +29,8 @@ public class River {
   public void setTarget(final int target) {
     this.target = target;
   }
+
+  public HashMap<String, Double> getAlgorithmWeights() { return algorithmWeights; }
+
+  public void setAlgorithmWeights(final HashMap<String, Double> algorithmWeights) { this.algorithmWeights = algorithmWeights; }
 }
