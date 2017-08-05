5267d913f83364cc9e3e9d9c5393b52f8723d7eb
tim <teemobean@gmail.com>
2017-08-04 22:53:26 -0700

    Add tests.

--- src/test/java/com/godaddy/icfp2017/GameLogicTests.java
+++ src/test/java/com/godaddy/icfp2017/GameLogicTests.java
@@ -1,10 +1,11 @@
 package com.godaddy.icfp2017;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.godaddy.icfp2017.models.GameplayP2S;
-import com.godaddy.icfp2017.models.GameplayS2P;
-import com.godaddy.icfp2017.models.SetupS2P;
+import com.godaddy.icfp2017.models.*;
+import com.godaddy.icfp2017.services.Algorithms;
 import com.godaddy.icfp2017.services.GameLogic;
+import com.godaddy.icfp2017.services.Weights;
+import org.jgrapht.graph.SimpleWeightedGraph;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -20,6 +21,27 @@ public class GameLogicTests {
     final GameplayP2S move = impl.move(loadMoves());
 
     assertThat(move).isNotNull();
+  }
+
+  @Test
+  public void run_game_and_one_move_with_adjacent_mines() throws IOException {
+    GameLogic impl = new GameLogic();
+    impl.setup(loadSetup());
+    final GameplayP2S move = impl.move(loadMoves());
+    SimpleWeightedGraph<Site, River> graph = move.getState().getMap();
+
+    graph.vertexSet()
+            .forEach(site -> {
+              graph.edgesOf(site)
+                      .forEach(river -> {
+                        if (site.isMine()) {
+                            assertThat(river.getAlgorithmWeights().get(Algorithms.Adjacent) == Weights.Max);
+                        }
+                        else {
+                            assertThat(river.getAlgorithmWeights().get(Algorithms.Adjacent) == Weights.Identity);
+                        }
+                      });
+            });
   }
 
   private SetupS2P loadSetup() throws IOException {
