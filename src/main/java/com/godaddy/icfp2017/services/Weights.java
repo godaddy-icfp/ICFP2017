0d4e83e1471c09e3ff2feb50b02b10b0e326dc31
John Kercheval <jkercheval@godaddy.com>
2017-08-05 01:55:21 -0700

    Move weights to be smaller to avoid sizing errors

--- src/main/java/com/godaddy/icfp2017/services/Weights.java
+++ src/main/java/com/godaddy/icfp2017/services/Weights.java
@@ -4,10 +4,10 @@ public class Weights {
   private Weights() {
   }
 
-  public static final double Max = 10.0;
-  public static final double HighlyDesired = 7.0;
-  public static final double Desired = 5.0;
-  public static final double Decent = 3.0;
+  public static final double Max = 2.0;
+  public static final double HighlyDesired = 1.7;
+  public static final double Desired = 1.5;
+  public static final double Decent = 1.3;
   public static final double Identity = 1.0;
   public static final double Zero = 0;
 }
