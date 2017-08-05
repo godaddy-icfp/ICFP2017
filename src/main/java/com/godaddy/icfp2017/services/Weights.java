eb73bf632f939cf3f769174e083e11d27104f2d3
John Kercheval <jkercheval@godaddy.com>
2017-08-05 00:05:49 -0700

    Add more fine grained weight

--- src/main/java/com/godaddy/icfp2017/services/Weights.java
+++ src/main/java/com/godaddy/icfp2017/services/Weights.java
@@ -5,6 +5,9 @@ public class Weights {
   }
 
   public static final double Max = 10.0;
+  public static final double HighlyDesired = 7.0;
+  public static final double Desired = 5.0;
+  public static final double Decent = 3.0;
   public static final double Identity = 1.0;
   public static final double Zero = 0;
 }
