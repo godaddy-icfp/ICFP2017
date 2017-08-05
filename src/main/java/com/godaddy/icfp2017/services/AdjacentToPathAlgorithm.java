2289e35d22366eeb651054a48e3d6ec9b28ffbcf
John Kercheval <jkercheval@godaddy.com>
2017-08-05 00:21:51 -0700

    add edge path lengthen algorithm

---
+++ src/main/java/com/godaddy/icfp2017/services/AdjacentToPathAlgorithm.java
@@ -0,0 +1,44 @@
+package com.godaddy.icfp2017.services;
+
+import com.godaddy.icfp2017.models.River;
+import com.godaddy.icfp2017.models.Site;
+import com.godaddy.icfp2017.models.State;
+import org.jgrapht.graph.SimpleWeightedGraph;
+
+public class AdjacentToPathAlgorithm implements GraphAlgorithm {
+  private final SimpleWeightedGraph<Site, River> graph;
+  private final State state;
+
+  public AdjacentToPathAlgorithm(final State state) {
+    this.graph = state.getMap();
+    this.state = state;
+  }
+
+  public void run() {
+    this.graph.edgeSet()
+              .forEach(river -> {
+                boolean sourceConnected = false;
+                boolean targetConnected = false;
+
+                Site source = graph.getEdgeSource(river);
+                for(River sourceRiver: graph.edgesOf(source)) {
+                  if (sourceRiver.getClaimedBy() == this.state.getPunter()) {
+                    sourceConnected = true;
+                  }
+                }
+
+                Site target = graph.getEdgeTarget(river);
+                for(River targetRiver: graph.edgesOf(target)) {
+                  if (targetRiver.getClaimedBy() == this.state.getPunter()) {
+                    targetConnected = true;
+                  }
+                }
+
+                if (sourceConnected && targetConnected) {
+                  river.getAlgorithmWeights().put(Algorithms.Adjacent, Weights.Decent);
+                } else if (sourceConnected || targetConnected) {
+                  river.getAlgorithmWeights().put(Algorithms.Adjacent, Weights.Desired);
+                }
+              });
+  }
+}
