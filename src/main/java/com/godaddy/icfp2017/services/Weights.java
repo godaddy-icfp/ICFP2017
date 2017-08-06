c89e9f575eb3f58b1aee26bb97a04a795795a4cc
John Kercheval <jkercheval@godaddy.com>
2017-08-06 12:24:12 -0700

    Update weight to probablistic ranges

--- src/main/java/com/godaddy/icfp2017/services/Weights.java
+++ src/main/java/com/godaddy/icfp2017/services/Weights.java
@@ -4,11 +4,11 @@ public class Weights {
   private Weights() {
   }
 
-  public static final double Max = 2.0;
-  public static final double HighlyDesired = 1.7;
-  public static final double Desired = 1.5;
-  public static final double Decent = 1.3;
-  public static final double Identity = 1.0;
+  public static final double Max = 1.0;
+  public static final double HighlyDesired = 0.9;
+  public static final double Desired = 0.75;
+  public static final double Decent = 0.6;
+  public static final double Identity = 0.5;
   public static final double Undesireable = 0.001;
   public static final double Zero = 0;
 }
