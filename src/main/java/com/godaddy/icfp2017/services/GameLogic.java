0778dff51c8be55679d077e5e0d9678ff34e36af
John Kercheval <jkercheval@godaddy.com>
2017-08-06 20:00:19 -0700

    Move static test method over to test module

--- src/main/java/com/godaddy/icfp2017/services/GameLogic.java
+++ src/main/java/com/godaddy/icfp2017/services/GameLogic.java
@@ -1,17 +1,7 @@
 package com.godaddy.icfp2017.services;
 
 import com.godaddy.icfp2017.models.*;
-import com.google.common.collect.ImmutableMap;
-import com.google.common.collect.ImmutableSet;
-import org.jgrapht.graph.SimpleWeightedGraph;
-import org.jgrapht.graph.builder.UndirectedWeightedGraphBuilderBase;
-
 import java.io.PrintStream;
-import java.util.List;
-import java.util.function.Function;
-
-import static com.google.common.collect.ImmutableMap.toImmutableMap;
-import static com.google.common.collect.ImmutableSet.toImmutableSet;
 
 public class GameLogic {
 
@@ -32,36 +22,6 @@ public class GameLogic {
     response.setReady(setup.getPunter());
     response.setState(this.currentState);
     return response;
-  }
-
-  static Pair<ImmutableSet<Site>, SimpleWeightedGraph<Site, River>> buildGraph(
-      final SetupS2P setup) {
-    final Map map = setup.getMap();
-    final List<Site> sites = map.getSites();
-    final List<River> rivers = map.getRivers();
-    final ImmutableSet<Integer> mines = ImmutableSet.copyOf(map.getMines());
-    final ImmutableMap<Integer, Site> siteById = sites
-        .stream()
-        .collect(toImmutableMap(Site::getId, Function.identity()));
-
-    final UndirectedWeightedGraphBuilderBase<Site, River, ? extends SimpleWeightedGraph<Site, River>, ?> builder =
-        SimpleWeightedGraph.builder(new LambdaEdgeFactory());
-
-    for (final Site site : sites) {
-      site.setMine(mines.contains(site.getId()));
-      builder.addVertex(site);
-    }
-
-    for (final River river : rivers) {
-      builder.addEdge(
-          siteById.get(river.getSource()),
-          siteById.get(river.getTarget()),
-          river);
-    }
-
-    return Pair.of(
-        mines.stream().map(siteById::get).collect(toImmutableSet()),
-        builder.build());
   }
 
   public GameplayP2S move(final GameplayS2P move) {
