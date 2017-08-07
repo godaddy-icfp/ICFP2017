4e4bf84ebfc2e91601ea14cfeca1cc798b829c74
John Kercheval <jkercheval@godaddy.com>
2017-08-06 22:47:47 -0700

    Add analyzer for strategy

--- src/main/java/com/godaddy/icfp2017/services/GameAnalyzer.java
+++ src/main/java/com/godaddy/icfp2017/services/GameAnalyzer.java
@@ -5,6 +5,7 @@ import com.godaddy.icfp2017.services.analysis.Analyzers;
 import com.godaddy.icfp2017.services.analysis.GraphAnalyzer;
 import com.godaddy.icfp2017.services.analysis.MineToMinePathAnalyzer;
 import com.godaddy.icfp2017.services.analysis.SiteConnectivityAnalyzer;
+import com.godaddy.icfp2017.services.analysis.StrategyAnalyzer;
 import com.google.common.collect.ImmutableMap;
 
 public class GameAnalyzer {
@@ -12,7 +13,8 @@ public class GameAnalyzer {
   private final ImmutableMap<Analyzers, GraphAnalyzer> analyzers =
       ImmutableMap.of(
           Analyzers.SiteConnectivity, new SiteConnectivityAnalyzer(),
-          Analyzers.MineToMinePath, new MineToMinePathAnalyzer()
+          Analyzers.MineToMinePath, new MineToMinePathAnalyzer(),
+          Analyzers.Strategy, new StrategyAnalyzer() // Must run after SiteConnectivity
       );
 
   public void run(State state) {
--- src/main/java/com/godaddy/icfp2017/services/analysis/Analyzers.java
+++ src/main/java/com/godaddy/icfp2017/services/analysis/Analyzers.java
@@ -2,7 +2,8 @@ package com.godaddy.icfp2017.services.analysis;
 
 public enum Analyzers implements Timeable {
   SiteConnectivity,
-  MineToMinePath;
+  MineToMinePath,
+  Strategy;
 
   @Override
   public String timeKey() {
