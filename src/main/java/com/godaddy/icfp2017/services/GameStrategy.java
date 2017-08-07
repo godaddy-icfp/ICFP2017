cdb8c40ba4ec0cf7f107bc05006cfb9363a5012c
John Kercheval <jkercheval@godaddy.com>
2017-08-06 21:52:00 -0700

    Minor changes and third strategy

--- src/main/java/com/godaddy/icfp2017/services/GameStrategy.java
+++ src/main/java/com/godaddy/icfp2017/services/GameStrategy.java
@@ -8,7 +8,7 @@ public class GameStrategy {
 
   // These are constants that value algorithms over all rivers
   // It allows us to select which algorithms are valuable (and which are not) for this particular move
-  private final ImmutableMap<Algorithms, Double> algorithmValuesMineAcquire = ImmutableMap.<Algorithms, Double>builder()
+  private final ImmutableMap<Algorithms, Double> mineAcquireStrategy = ImmutableMap.<Algorithms, Double>builder()
       .put(Algorithms.AdjacentToMine, 0.8)
       .put(Algorithms.AdjacentToPath, 0.25)
       .put(Algorithms.ConnectedDecision, 0.25)
@@ -18,7 +18,17 @@ public class GameStrategy {
       .put(Algorithms.ScoringAlgo, 0.8)
       .build();
 
-  private final ImmutableMap<Algorithms, Double> algorithmValuesProgress = ImmutableMap.<Algorithms, Double>builder()
+  private final ImmutableMap<Algorithms, Double> minimumSpanningTreeStrategy = ImmutableMap.<Algorithms, Double>builder()
+      .put(Algorithms.AdjacentToMine, 0.8)
+      .put(Algorithms.AdjacentToPath, 0.25)
+      .put(Algorithms.ConnectedDecision, 0.25)
+      .put(Algorithms.Connectedness, 0.25)
+      .put(Algorithms.MineToMine, 1.0)
+      .put(Algorithms.MinimumSpanningTree, 0.8)
+      .put(Algorithms.ScoringAlgo, 0.8)
+      .build();
+
+  private final ImmutableMap<Algorithms, Double> pathExtendStrategy = ImmutableMap.<Algorithms, Double>builder()
       .put(Algorithms.AdjacentToMine, 0.5)
       .put(Algorithms.AdjacentToPath, 0.5)
       .put(Algorithms.ConnectedDecision, 0.25)
@@ -28,13 +38,13 @@ public class GameStrategy {
       .put(Algorithms.ScoringAlgo, 1.0)
       .build();
 
-  private ImmutableMap<Algorithms, Double> strategyState = algorithmValuesMineAcquire;
+  private ImmutableMap<Algorithms, Double> strategyState = mineAcquireStrategy;
 
   public ImmutableMap<Algorithms, Double> getStrategy(State state) {
     if (!mineAdjacenciesExist(state)) {
-      return algorithmValuesMineAcquire;
+      return mineAcquireStrategy;
     }
-    return algorithmValuesProgress;
+    return pathExtendStrategy;
   }
 
   private boolean mineAdjacenciesExist(State state) {
